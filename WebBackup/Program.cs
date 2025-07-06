978509b2a97fe84c07cab866a40b38bd70555e15
Pavel Martinec <martinec98@gmail.com>
2025-07-06 21:00:07 +0200

    Use file scoped namespaces.

--- WebBackup/Program.cs
+++ WebBackup/Program.cs
@@ -1,31 +1,30 @@
 ﻿using Microsoft.Extensions.Configuration;
 
-namespace WebBackup
+namespace WebBackup;
+
+public class Program
 {
-    public class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        var config = LoadConfiguration();
+        var webSettings = config.GetSection("WebFtpSettings").Get<WebFtpSettings[]>() ?? [];
+        var userSelectedWebs = UserInteraction.PromptUserForWebSelection(webSettings);
+        using CancellationTokenSource tokenSource = new();
+        Console.CancelKeyPress += (s, e) =>
         {
-            var config = LoadConfiguration();
-            var webSettings = config.GetSection("WebFtpSettings").Get<WebFtpSettings[]>() ?? [];
-            var userSelectedWebs = UserInteraction.PromptUserForWebSelection(webSettings);
-            using CancellationTokenSource tokenSource = new();
-            Console.CancelKeyPress += (s, e) =>
-            {
-                tokenSource.Cancel();
-                e.Cancel = true;
-            };
+            tokenSource.Cancel();
+            e.Cancel = true;
+        };
 
-            var websWithPassword = await UserInteraction.CollectPasswordsAsync(userSelectedWebs, tokenSource.Token);
+        var websWithPassword = await UserInteraction.CollectPasswordsAsync(userSelectedWebs, tokenSource.Token);
 
-            await BackupManager.PerformBackup(websWithPassword, tokenSource.Token);
+        await BackupManager.PerformBackup(websWithPassword, tokenSource.Token);
 
-            UserInteraction.DisplayBackupStatus(websWithPassword, tokenSource.IsCancellationRequested);
-        }
-
-        private static IConfiguration LoadConfiguration() => new ConfigurationBuilder()
-            .AddJsonFile($"appsettings.json")
-            .AddUserSecrets<Program>()
-            .Build();
+        UserInteraction.DisplayBackupStatus(websWithPassword, tokenSource.IsCancellationRequested);
     }
+
+    private static IConfiguration LoadConfiguration() => new ConfigurationBuilder()
+        .AddJsonFile($"appsettings.json")
+        .AddUserSecrets<Program>()
+        .Build();
 }
