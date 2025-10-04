20e358621a765eed6632142ab40f1f4c95de2d50
Pavel Martinec <martinec98@gmail.com>
2025-10-04 16:43:06 +0200

    Created functions project for AzureStaticWebApps

---
+++ Tlumacov.OfficialBoard.Api/Functions.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Tlumacov.OfficialBoard.Api;
+
+public class Functions
+{
+    private readonly ILogger<Functions> _logger;
+
+    public Functions(ILogger<Functions> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("Functions")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
